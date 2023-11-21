20d563deb9844cc74b302927b04ee9d3274b09ed
BvrWebmaster <webmaster1@bvrgroupasia.com>
2023-11-21 12:11:15 +0800

    update jakarta

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="Experience the beauty of Bali in every sip of your Bali Arabica coffee, special at our coffee shop in Jakarta. Presented by Bali Coffee Cup Jakarta">
-        <meta name="keywords" content="coffee shop jakarta, cafe jakarta, arabica coffee beans, vegan cafe jakarta, arabica coffee jakarta, bali arabica coffee, arabica espresso,arabica specialty coffee">
+        <meta name="keywords" content="coffee shop jakarta | cafe jakarta | arabica coffee beans | vegan cafe jakarta | arabica coffee jakarta | bali arabica coffee | arabica espresso | arabica specialty coffee">
         <link rel="apple-touch-icon" sizes="180x180" href="{{ asset("/images/apple-touch-icon.png") }}">
         <link rel="icon" type="image/png" sizes="32x32" href="{{ asset("/images/favicon-32x32.png") }}">
         <link rel="icon" type="image/png" sizes="16x16" href="{{ asset("/images/favicon-16x16.png") }}">
